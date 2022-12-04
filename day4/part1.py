7afcb98d0085392e1ef42895a821fb7120ea0025
dpw97 <dweaver1017@gmail.com>
2022-12-04 11:45:57 -0800

    new file:   day4/file.txt new file:   day4/part1.py new file:   day4/part2.py

---
+++ day4/part1.py
@@ -0,0 +1,15 @@
+with open('file.txt','r') as file:
+    total = 0
+    for line in file:
+        l = line.split(",")
+        e1 = l[0].split("-")
+        e2 = l[1].rstrip('\n').split("-")
+
+        e1Start, e1End = int(e1[0]), int(e1[1])
+        e2Start, e2End = int(e2[0]), int (e2[1])
+
+        if e1Start >= e2Start and e1End <= e2End:
+            total+=1
+        elif e2Start >= e1Start and e2End <= e1End:
+            total+=1
+print(total)
